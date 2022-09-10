a4c548395b89836f3f826ae0931a3ad8c46fadf4
Int-Angel <Padilla.angel88@gmail.com>
2022-09-10 15:51:32 -0500

    Server connection

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const PORT = 8800;
+
+const connect = ()=>{
+    mongoose.connect(process.env.MONGO).then(() =>{
+        console.log("Connected to DB");
+    })
+    .catch(err =>{
+        throw err;
+    })
+}
+
+app.listen(PORT, ()=> {
+    connect();
+    console.log("Listening to port: " + PORT);
+})
