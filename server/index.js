2d37a726a707473a384c67af32e7ca6a97e991ab
Int-Angel <Padilla.angel88@gmail.com>
2022-09-10 17:17:17 -0500

    User schema

--- server/index.js
+++ server/index.js
@@ -7,16 +7,18 @@ dotenv.config();
 
 const PORT = 8800;
 
-const connect = ()=>{
-    mongoose.connect(process.env.MONGO).then(() =>{
-        console.log("Connected to DB");
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+      console.log("Connected to DB");
     })
-    .catch(err =>{
-        throw err;
-    })
-}
+    .catch((err) => {
+      throw err;
+    });
+};
 
-app.listen(PORT, ()=> {
-    connect();
-    console.log("Listening to port: " + PORT);
-})
+app.listen(PORT, () => {
+  connect();
+  console.log("Listening to port: " + PORT);
+});
